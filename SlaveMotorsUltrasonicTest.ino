fc7408015046a97d0a483af766c54da0ba9c5d5a
BrinzasVlad <44496910+BrinzasVlad@users.noreply.github.com>
2019-01-18 17:35:29 +0200

    Add files via upload

---
+++ SlaveMotorsUltrasonicTest.ino
@@ -0,0 +1,188 @@
+#include <Wire.h>
+#include <Servo.h>
+
+/*
+ * Commands table:
+ */
+#define STOP 0
+#define FWD 1
+#define BACK 2
+#define LT 3
+#define RT 4
+
+//Slave address at 9
+#define SLAVE 9
+
+// Global variable for the command we read via I2C
+volatile int command = 0;
+
+// Global variable for the distance detected via ultrasonics
+volatile int distance = 100;
+int duration; //This is a variable to be used in computing distance
+
+//If the distance is this or lower, STOP
+#define limitDistance 15
+
+
+// Pins of motor 1
+#define mpin00 5
+#define mpin01 6
+
+// Pins of motor 2
+#define mpin10 3
+#define mpin11 11
+
+Servo srv;
+#define servoPin 8
+
+
+// Pins for ultrasonic sensor
+#define trigPin 10
+#define echoPin 9
+
+void setup() {
+    Serial.begin(9600);
+    
+    // configuration of motor pins as output, initially 0
+    digitalWrite(mpin00, 0);
+    digitalWrite(mpin01, 0);
+    digitalWrite(mpin10, 0);
+    digitalWrite(mpin11, 0);
+    
+    pinMode (mpin00, OUTPUT);
+    pinMode (mpin01, OUTPUT);
+    pinMode (mpin10, OUTPUT);
+    pinMode (mpin11, OUTPUT);
+    
+    // LED pin
+    pinMode(13, OUTPUT);
+
+    // Set servo so that we're looking forward (at least in theory)
+    srv.attach(servoPin);
+    srv.write(90); // This should be in the middle; change if needed, I s'pose
+    srv.detach();
+
+    
+    // LED warning code for things being about to happen
+    // Slow blink
+    for (int i=0; i<10; i++) //2 seconds
+    {
+        digitalWrite(13, 1);
+        delay(200);
+        digitalWrite(13, 0);
+        delay(200);
+    }
+    
+    // Fast blink. Remove the USB cable!!!!
+    for (int i=0; i<10; i++) //1 second
+    {
+        digitalWrite(13, 1);
+        delay(100);
+        digitalWrite(13, 0);
+        delay(100);
+    }
+    
+    digitalWrite(13, 1);
+
+    
+
+    // Now open up as slave and start receiving commands
+    Wire.begin(SLAVE);
+    Wire.onReceive(receiveEvent);
+}
+
+// Function to control a motor
+// Input: pins m1 and m2, direction and speed
+void StartMotor (int m1, int m2, int forward, int speed)
+{
+
+    if (speed==0) // stop
+    {
+        digitalWrite(m1, 0);
+        digitalWrite(m2, 0);
+    }
+    else
+    {
+        if (forward)
+        {
+            digitalWrite(m2, 0);
+            analogWrite (m1, speed); // use PWM
+        }
+        else
+        {
+            digitalWrite(m1, 0);
+            analogWrite(m2, speed);
+        }
+    }
+}
+
+void receiveEvent(int bytes) {
+    command = Wire.read(); //read I2C received command
+    switch (command) {
+        case FWD:
+            Serial.print("FWD\n");
+            Serial.print("\nDistance was ");
+            Serial.print(distance);
+            Serial.print("\n\n");
+			//if (distance <= limitDistance) { //Stop when commanded to move into obstacles
+			//	command = STOP;
+			//	StartMotor (mpin00, mpin01, 0, 0);
+			//	StartMotor (mpin10, mpin11, 0, 0);
+			//}
+            //else {
+                StartMotor (mpin00, mpin01, 1, 128);
+                StartMotor (mpin10, mpin11, 1, 128);
+            //}
+            break;
+        case BACK:
+            Serial.print("BACK\n");
+            StartMotor (mpin00, mpin01, 0, 128);
+            StartMotor (mpin10, mpin11, 0, 128);
+            break;
+        case LT:
+            Serial.print("LEFT\n");
+            StartMotor (mpin00, mpin01, 0, 128);
+            StartMotor (mpin10, mpin11, 1, 128);
+            break;
+        case RT:
+            Serial.print("RIGHT\n");
+            StartMotor (mpin00, mpin01, 1, 128);
+            StartMotor (mpin10, mpin11, 0, 128);
+            break;
+        default: //STOP is default behaviour
+            Serial.print("STOP\n");
+            StartMotor (mpin00, mpin01, 0, 0);
+            StartMotor (mpin10, mpin11, 0, 0);
+            break;
+    }
+}
+
+void loop() {    
+    // Clears the trigPin for ultrasonic sensor
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    
+    // Sets the trigPin on HIGH state for 10 micro seconds (launch sensing)
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+    
+    // Calculating the distance
+    distance = duration*0.034/2;
+    
+    Serial.print("\nDistance was ");
+    Serial.print(distance);
+    Serial.print("\n\n");
+
+    // If the distance is too short, STOP (to prevent collisions)
+    if(distance <= limitDistance
+       && command == FWD
+       && distance != 0) {
+        command = STOP;
+        StartMotor (mpin00, mpin01, 0, 0);
+        StartMotor (mpin10, mpin11, 0, 0);
+    }
+}
---
+++ WiFiMasterTest.ino
@@ -0,0 +1,112 @@
+#include <Wire.h>
+#include <LiquidCrystal.h>
+#define DEBUG true
+#define SLAVE 9
+
+//LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
+/*
+ * Commands table:
+ */
+#define STOP 0
+#define FWD 1
+#define BACK 2
+#define LT 3
+#define RT 4
+
+void setup() {
+    Wire.begin(); //Start as Master, slave at address 9 as per above
+    //lcd.begin(16, 2);
+    
+    Serial.begin(115200);
+    Serial1.begin(115200);
+    sendData("AT+RST\r\n", 2000, false); // reset module
+    sendData("AT+CWMODE=2\r\n", 1000, false); // configure as access point
+    sendData("AT+CIFSR\r\n", 1000, DEBUG); // get ip address
+    sendData("AT+CWSAP?\r\n", 2000, DEBUG); // get SSID info (network name)
+    sendData("AT+CIPMUX=1\r\n", 1000, false); // configure for multiple connections
+    sendData("AT+CIPSERVER=1,80\r\n", 1000, false); // turn on server on port 80
+}
+
+void loop() {
+    if (Serial1.available()) {
+        if (Serial1.find("+IPD,")) {
+            delay(500);
+            int connectionId = Serial1.read() - 48; // read() function returns
+            // ASCII decimal value and 0 (the first decimal number) starts at 48
+            
+            String webpage = "<h1>Command Screen!</h1>";
+            webpage += "<p><a href=\"/l0\"><button style=\"height:100px;width:150px\">FWD</button></a></p>";
+            webpage += "<p><a href=\"/l1\"><button style=\"height:100px;width:150px\">STOP</button></a></p>";
+            webpage += "<p><a href=\"/l2\"><button style=\"height:100px;width:150px\">BACK</button></a></p>";
+            webpage += "<p><a href=\"/l3\"><button style=\"height:100px;width:150px\">LT</button></a></p>";
+            webpage += "<p><a href=\"/l4\"><button style=\"height:100px;width:150px\">RT</button></a></p>";
+            
+            String cipSend = "AT+CIPSEND=";
+            cipSend += connectionId;
+            cipSend += ",";
+            
+            cipSend += webpage.length();
+            cipSend += "\r\n";
+            sendData(cipSend, 100, DEBUG);
+            sendData(webpage, 150, DEBUG);
+
+            String closeCommand = "AT+CIPCLOSE=";
+            closeCommand += connectionId; // append connection id
+            closeCommand += "\r\n";
+            sendData(closeCommand, 300, DEBUG);
+        }
+    }
+}
+
+String sendData(String command, const int timeout, boolean debug) {
+    String response = "";
+    Serial1.print(command); // send command to the esp8266
+    long int time = millis();
+    while ((time + timeout) > millis()) {
+        while (Serial1.available()) {
+            char c = Serial1.read(); // read next char
+            response += c;
+        }
+    }
+
+    if (response.indexOf("/l0") != -1) {
+        //lcd.clear();
+        //lcd.print("FWD");
+        sendCommand(FWD);
+    }
+    if (response.indexOf("/l1") != -1) {
+        //lcd.clear();
+        //lcd.print("STOP");
+        sendCommand(STOP);
+    }
+    if (response.indexOf("/l2") != -1) {
+        //lcd.clear();
+        //lcd.print("BACK");
+        sendCommand(BACK);
+    }
+    if (response.indexOf("/l3") != -1) {
+        //lcd.clear();
+        //lcd.print("LT");
+        sendCommand(LT);
+    }
+    if (response.indexOf("/l4") != -1) {
+        //lcd.clear();
+        //lcd.print("RT");
+        sendCommand(RT);
+    }
+    if (debug) {
+        Serial.print(response);
+    }
+    return response;
+}
+
+void sendCommand(int command) {
+    Wire.beginTransmission(SLAVE);
+    Serial.print("\n\n\nI am sending command ");
+    Serial.print(command);
+    Serial.print("\n\n\n");
+    Wire.write(command);
+    Wire.endTransmission();
+}
+
